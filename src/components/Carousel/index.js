5c81918151c25f48b3ae9a739da1439c7ea0a107
dorian-morones <dorianm393@gmail.com>
2020-12-12 22:01:59 -0800

    carousel working

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,32 +1,41 @@
 import * as React from "react";
-import { CarouselContainer } from "./styles/carousel";
+import { 
+  CarouselContainer,
+  ImageContainer,
+  CloseButton,
+  Controls,
+  Arrow
+} from "./styles/carousel";
 
 const Carousel = ({ onClose, images }) => {
-
   const [position, setPosition] = React.useState(0);
 
   const handleNext = () => {
-    if(position < (images.length - 1)){
-      setPosition(position + 1)
+    if (position < images.length - 1) {
+      setPosition(position + 1);
     }
   };
 
   const handlePrev = () => {
-    if(position > 0){
-      setPosition(position - 1)
+    if (position > 0) {
+      setPosition(position - 1);
     }
   };
 
   return (
     <CarouselContainer>
-      <p onClick={() => onClose()}>x</p>
-      <img src={images[position]?.href} alt="product_Image" />
-      {/* {images !== undefined &&
+      <CloseButton onClick={() => onClose()}>x</CloseButton>
+      <ImageContainer>
+        <img src={images[position]?.href} alt="product_Image" />
+        {/* {images !== undefined &&
         images.map((item) => {
           return <img src={item?.href} />;
         })} */}
-        <p onClick={() => handlePrev()}>Prev</p>
-        <p onClick={() => handleNext()}>Next</p>
+        <Controls>
+          <Arrow onClick={() => handlePrev()}>Prev</Arrow>
+          <Arrow onClick={() => handleNext()}>Next</Arrow>
+        </Controls>
+      </ImageContainer>
     </CarouselContainer>
   );
 };
--- src/components/Carousel/styles/carousel.js
+++ src/components/Carousel/styles/carousel.js
@@ -6,8 +6,43 @@ export const CarouselContainer = styled.div.attrs(() => ({
   position: absolute;
   width: 100vw;
   height: 100vh;
-  background: rgb(156 156 156 / 20%);
+  background: rgb(0 0 0 / 30%);
   left: 0;
   top: 0;
-  overflow: hidden
+  overflow: hidden;
 `;
+
+export const CloseButton = styled.div.attrs(() => ({
+  className: "CloseButton",
+}))`
+  position: absolute;
+  right: 40px;
+  top: 20px;
+  background: #fff;
+  padding: 15px;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+`;
+
+export const ImageContainer = styled.div.attrs(() => ({
+  className: "ImageContainer",
+}))`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  justify-content: center;
+  margin: 0 auto;
+`;
+
+export const Controls = styled.div.attrs(() => ({
+  className: "Controls",
+}))`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Arrow = styled.div.attrs(() => ({
+  className: "Arrow",
+}))``;
