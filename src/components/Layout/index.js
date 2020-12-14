56fb16b5d3c3efcfb8b8ecb4e4265bfe057a1091
dorian-morones <dorianm393@gmail.com>
2020-12-13 21:44:59 -0800

    SEO helmet updates

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,21 +1,32 @@
 import * as React from "react";
-import Logo from '../../images/logo.png';
+import Logo from "../../images/logo.png";
+import { Helmet } from "react-helmet";
 
 // styles
-import {
-  Menu,
-  LogoImage
-} from './styles/layout'
+import { Menu, LogoImage } from "./styles/layout";
 
-const Layout = ({children}) => {
-
+const Layout = ({ children }) => {
   return (
-    <div data-testid='layout-item'>
-      <Menu><LogoImage src={Logo} alt={Logo} /></Menu>
+    <div data-testid="layout-item">
+      <Helmet>
+        {/* General tags */}
+        <title>williams sonoma light</title>
+        <meta name="description" content="Small product page" />
+        {/* OpenGraph tags */}
+        <meta property="og:title" content='Williams sonoma light' />
+        <meta property="og:description" content="Small product page" />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content='Williams sonoma light' />
+        <meta name="twitter:description" content="Small product page" />
+      </Helmet>
+      <Menu>
+        <LogoImage src={Logo} alt={Logo} />
+      </Menu>
       <main>{children}</main>
     </div>
-  )
-
-}
+  );
+};
 
 export default Layout;
